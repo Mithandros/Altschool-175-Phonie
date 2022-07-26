98d0c0bff54fcd0c29d0cef88094d598bd16083f
Hammed Owoade <35741417+miidiin10@users.noreply.github.com>
2022-07-26 12:29:22 +0000

    i included the logos for each telecom and a check button to identify the company's digit that is being inputed

--- index.mjs
+++ index.mjs
@@ -1,3 +1,29 @@
 import startApp from './app.mjs';
 
 document.addEventListener('DOMContentLoaded', startApp); 
+
+
+const mtn = document.querySelector('#mtn')
+const glo = document.querySelector('#glo')
+const etisalat = document.querySelector('#etisalat')
+const airtel = document.querySelector('#airtel')
+
+document.querySelector('#check-logo').addEventListener('click', showTelecomLogo);
+
+
+function showTelecomLogo() {
+    let getVal = document.getElementById('phone').value;
+    let getPhoneCode = getVal.substr(0,4);
+    
+    
+    if (getPhoneCode == "0803"){
+        mtn.classList.toggle('hidden')
+    }else if (getPhoneCode == "0805") {
+        glo.classList.toggle('hidden')
+    }else if (getPhoneCode == "0809") {
+        etisalat.classList.toggle('hidden')
+    }else if (getPhoneCode == "0802") {
+        airtel.classList.toggle('hidden')
+    }
+        
+}
