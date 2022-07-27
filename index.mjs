1df093e55af285d87709124195a57c931f995537
Hammed Owoade <35741417+miidiin10@users.noreply.github.com>
2022-07-27 00:38:17 +0000

    i changed the logos to appear once the condition set tallies and make the remaining set of logos dissapear

--- index.mjs
+++ index.mjs
@@ -16,14 +16,26 @@ function showTelecomLogo() {
     let getPhoneCode = getVal.substr(0,4);
     
     
-    if (getPhoneCode == "0803"){
+    if (getPhoneCode == "0803" || getPhoneCode == "0806" || getPhoneCode == "0816" || getPhoneCode == "0810" || getPhoneCode == "0813" || getPhoneCode == "0814" || getPhoneCode == "0903" || getPhoneCode == "0906" || getPhoneCode == "0703" || getPhoneCode == "0706"){
         mtn.classList.toggle('hidden')
-    }else if (getPhoneCode == "0805") {
+        glo.classList.add('hidden')
+        etisalat.classList.add('hidden')
+        airtel.classList.add('hidden')
+    }else if (getPhoneCode == "0805" || getPhoneCode == "0905" ||getPhoneCode == "0807" || getPhoneCode == "0811" || getPhoneCode == "0705" || getPhoneCode == "0815") {
         glo.classList.toggle('hidden')
-    }else if (getPhoneCode == "0809") {
+        mtn.classList.add('hidden')
+        etisalat.classList.add('hidden')
+        airtel.classList.add('hidden')
+    }else if (getPhoneCode == "0809" || getPhoneCode == "0909" || getPhoneCode == "0908" || getPhoneCode == "0818" || getPhoneCode == "0817") {
         etisalat.classList.toggle('hidden')
-    }else if (getPhoneCode == "0802") {
+        mtn.classList.add('hidden')
+        glo.classList.add('hidden')
+        airtel.classList.add('hidden')
+    }else if (getPhoneCode == "0802" || getPhoneCode == "0907" || getPhoneCode == "0708" || getPhoneCode == "0902" || getPhoneCode == "0812" || getPhoneCode == "0808" || getPhoneCode == "0701") {
         airtel.classList.toggle('hidden')
+        mtn.classList.add('hidden')
+        glo.classList.add('hidden')
+        etisalat.classList.add('hidden')
     }
         
 }
